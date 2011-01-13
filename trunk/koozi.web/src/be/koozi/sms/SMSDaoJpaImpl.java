febddea4ae7fe21cbf7c2ace7e964e6b8e09b582
tola2000 <tola2000@6bd4741a-bca6-ad91-aea1-ec8b5917003e>
2011-01-13 17:25:06 +0000

    nc

--- trunk/koozi.web/src/be/koozi/sms/SMSDaoJpaImpl.java
+++ trunk/koozi.web/src/be/koozi/sms/SMSDaoJpaImpl.java
@@ -22,7 +22,7 @@ public class SMSDaoJpaImpl extends EntityDaoJpaImpl<SMS, Long> implements SMSDao
 	public List<SMS> findByUserId(String userId) {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("userId", userId);
-		return find(params);
+		return find(params, "timestamp ASC");
 	}
 
 }
